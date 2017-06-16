7d1a97bda2b47b05e83616eaaf81dad75ee5121b
Ruben Callewaert <rubencallewaertdev@gmail.com>
2017-06-17 00:23:26 +0200

    Update default value forging NO

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ sed -i -e "s/\"LISTENING_PORT\"  : \"8000\"/\"LISTENING_PORT\"  : \"$LISK_PORT\"
 sed -i -e "s/PICK_INSTANCE_NAME/$LISK_ADDRESS/g" app.json
 sed -i -e "s/\"CONTACT_DETAILS\" : \"\",/\"CONTACT_DETAILS\" : \"$LISK_CONTACT\"/g" app.json
 sed -i -e "s/Go to https:\/\/lisk.chat and ask around/$LISKSTATS_SECRET/g" app.json
-sed -i -e "s/\"IS_FORGING\"      : \"no\"/\"IS_FORGING\"      : \"$LISK_ISFORGING\"/g" app.json
+sed -i -e "s/\"IS_FORGING\"      : \"no\"/\"IS_FORGING\"      : \"${LISK_ISFORGING:-no}\"/g" app.json
 git pull
 npm install
 node app.js
