9ee45f4450047cf1dc1162e4f5520a14e0d7bb07
Ruben Callewaert <rubencallewaertdev@gmail.com>
2017-06-17 12:28:52 +0200

    Sane defaults

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i -e "s/\"RPC_HOST\"        : \"localhost\"/\"RPC_HOST\"        : \"$LISK_HOST\"/g" app.json
-sed -i -e "s/\"RPC_PORT\"        : \"8000\"/\"RPC_PORT\"        : \"$LISK_PORT\"/g" app.json
-sed -i -e "s/\"LISTENING_PORT\"  : \"8000\"/\"LISTENING_PORT\"  : \"$LISK_PORT\"/g" app.json
+sed -i -e "s/\"RPC_PORT\"        : \"8000\"/\"RPC_PORT\"        : \"${LISK_PORT:-8000}\"/g" app.json
+sed -i -e "s/\"LISTENING_PORT\"  : \"8000\"/\"LISTENING_PORT\"  : \"${LISK_PORT:-8000}\"/g" app.json
 sed -i -e "s/PICK_INSTANCE_NAME/$LISK_ADDRESS/g" app.json
 sed -i -e "s/\"CONTACT_DETAILS\" : \"\",/\"CONTACT_DETAILS\" : \"$LISK_CONTACT\"/g" app.json
 sed -i -e "s/Go to https:\/\/lisk.chat and ask around/$LISKSTATS_SECRET/g" app.json
